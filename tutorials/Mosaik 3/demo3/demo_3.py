6922292d28b98bffc4a6e1daeb47103b01d866d5
yanaspaula <yanaspaula@gmail.com>
2022-11-21 21:33:02 -0300

    UPDATE demo_3.py

--- tutorials/Mosaik 3/demo3/demo_3.py
+++ tutorials/Mosaik 3/demo3/demo_3.py
@@ -2,7 +2,7 @@
 import mosaik
 import mosaik.util
 
-# Sim config. and other parameters
+# Configuração do cenário
 SIM_CONFIG = {
     'ExampleSim': {
         'python': 'simulator_mosaik:ExampleSim',
@@ -19,34 +19,34 @@ SIM_CONFIG = {
 }
 END = 6  # 10 seconds
 
-# Create World
+# Criação do mundo
 world = mosaik.World(SIM_CONFIG)
 
-# Start simulators
+# Inicialização dos simuladores
 examplesim = world.start('ExampleSim', eid_prefix='Model_')
 examplectrl = world.start('ExampleCtrl')
 examplemasterctrl = world.start('ExampleMasterCtrl')
 collector = world.start('Collector')
 
-# Instantiate models
+# Criação das instâncias dos simuladores
 models = [examplesim.ExampleModel(init_val=i) for i in (-2, 0, -2)]
 agents = examplectrl.Agent.create(len(models))
 print('examplemasterctrl: {}'.format(examplemasterctrl))
 master_agent = examplemasterctrl.Agent.create(1)
 monitor = collector.Monitor()
 
-# Connect entities
+# Conexão das entidades
 for model, agent in zip(models, agents):
     world.connect(model, agent, ('val', 'val_in'))
     world.connect(agent, model, 'delta', weak=True)
 
 for agent in agents:
     world.connect(agent, master_agent[0], ('delta', 'delta_in'))
-    world.connect(master_agent[0], agent, ('delta_out', 'delta'), weak=True)
+    world.connect(master_agent[0], agent, ('delta_out', 'delta'), weak=True) # Necessário para same-time-loops
 
 mosaik.util.connect_many_to_one(world, models, monitor, 'val', 'delta')
 mosaik.util.connect_many_to_one(world, agents, monitor, 'delta')
 world.connect(master_agent[0], monitor, 'delta_out')
 
-# Run simulation
+# Execução da simulação
 world.run(until=END)
